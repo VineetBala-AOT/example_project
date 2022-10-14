00118c8eb34550dc31848d4242c5fbd52efdd85e
VineetBala-AOT <vineet.balachandran@aot-technologies.com>
2022-10-14 17:35:29 -0600

    fourth commit

--- example_project/example_repo/repo.py
+++ example_project/example_repo/repo.py
@@ -1,62 +1,25 @@
-import os
-from collections import Counter
+from dagster import op, job, repository
 
-from dagster_aws.s3 import s3_pickle_io_manager, s3_resource
-from dagster_celery_k8s import celery_k8s_job_executor
-from dagster_k8s import k8s_job_executor
+@op
+def hello():
+    """
+    An op definition. This example op outputs a single string.
 
-from dagster import In, config_from_files, file_relative_path, graph, op, repository
+    For more hints about writing Dagster ops, see our documentation overview on Ops:
+    https://docs.dagster.io/concepts/ops-jobs-graphs/ops
+    """
+    return "Hello, Dagster!"
 
+@job
+def say_hello_job():
+    """
+    A job definition. This example job has a single op.
 
-@op(ins={"word": In(str)}, config_schema={"factor": int})
-def multiply_the_word(context, word):
-    return word * context.solid_config["factor"]
-
-
-@op(ins={"word": In(str)})
-def count_letters(word):
-    return dict(Counter(word))
-
-
-@graph
-def example_graph():
-    count_letters(multiply_the_word())
-
-
-step_isolated_job = example_graph.to_job(
-    name="step_isolated_job",
-    resource_defs={"s3": s3_resource, "io_manager": s3_pickle_io_manager},
-    executor_def=k8s_job_executor,
-    config=config_from_files(
-        [
-            file_relative_path(__file__, os.path.join("..", "run_config", "k8s.yaml")),
-            file_relative_path(__file__, os.path.join("..", "run_config", "pipeline.yaml")),
-        ]
-    ),
-)
-
-celery_step_isolated_job = example_graph.to_job(
-    name="celery_step_isolated",
-    resource_defs={"s3": s3_resource, "io_manager": s3_pickle_io_manager},
-    executor_def=celery_k8s_job_executor,
-    config=config_from_files(
-        [
-            file_relative_path(__file__, os.path.join("..", "run_config", "celery_k8s.yaml")),
-            file_relative_path(__file__, os.path.join("..", "run_config", "pipeline.yaml")),
-        ]
-    ),
-)
-
-single_pod_job = example_graph.to_job(
-    name="single_pod_job",
-    config=config_from_files(
-        [
-            file_relative_path(__file__, os.path.join("..", "run_config", "pipeline.yaml")),
-        ]
-    ),
-)
-
+    For more hints on writing Dagster jobs, see our documentation overview on Jobs:
+    https://docs.dagster.io/concepts/ops-jobs-graphs/jobs-graphs
+    """
+    hello()
 
 @repository
-def example_repo():
-    return [single_pod_job, step_isolated_job, celery_step_isolated_job]
+def etl_project():
+    return [say_hello_job]
